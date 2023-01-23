d9b1771e239a4fbdade38e14ced11430dcc92a08
mettaneko <55776002+mettaneko@users.noreply.github.com>
2023-01-23 19:31:24 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,22 @@
+window.addEventListener('scroll', function(){
+    let progressbar = document.querySelector('#prgsBar2');
+
+    let siteHeight = document.documentElement.scrollHeight
+    let monitorY = document.documentElement.clientHeight
+
+    let w = window.pageYOffset / (siteHeight - monitorY) * 100
+
+    prgsBar2.style.width = w + "%"
+});
+
+wikievents.addEventListener('click', function () {
+    window.location.href = 'events.html';
+})
+
+wikibanlist.addEventListener('click', function () {
+    window.location.href = 'banlist.html';
+})
+
+wikistaff.addEventListener('click', function () {
+    window.location.href = 'staff.html';
+})
